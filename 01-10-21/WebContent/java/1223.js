7db2b37cdf1f744a1eb313375d72cb3359b7cf98
novak <novak@nova>
2021-10-22 12:46:15 +0400

    first commit

---
+++ 01-10-21/WebContent/java/1223.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+function sayHi()
+		{
+		//This is variable decleration and initialtion.
+		var name="kalaiii";
+		
+		//This is alert function used to print output.
+		alert(name);
+		}
---
+++ 01-10-21/WebContent/java/script1.js
@@ -0,0 +1,17 @@
+/**
+ * document type
+ */
+
+// single line comment
+
+/*
+ 	Multi line comment:
+	sayHi() is a function
+	which contains alert() function, with that we are display text message on web browser
+*/
+function sayHi()
+{
+	alert("Hi Buddy...!");
+}
+
+document.getElementById("xyz").onclick = sayHi;
